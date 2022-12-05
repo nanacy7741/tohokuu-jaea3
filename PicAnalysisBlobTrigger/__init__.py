915ee713d53da5c04fda61c4d56f8a7ceada4224
yosukeuchida <ee0200625@gmail.com>
2022-12-06 00:05:28 +0900

    first commit

---
+++ PicAnalysisBlobTrigger/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+import azure.functions as func
+
+
+def main(inputblob: func.InputStream, outputblob: func.Out[func.InputStream]):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {inputblob.name}\n"
+                 f"Blob Size: {inputblob.length} bytes")
+    outputblob.set(inputblob)
