7ad02c512727ebbd3ffab52d6495caa772d8a458
yosukeuchida <ee0200625@gmail.com>
2022-12-06 02:27:40 +0900

    [2022.12.06] log add

--- PicAnalysisBlobTrigger/__init__.py
+++ PicAnalysisBlobTrigger/__init__.py
@@ -7,4 +7,5 @@ def main(inputblob: func.InputStream, outputblob: func.Out[func.InputStream]):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {inputblob.name}\n"
                  f"Blob Size: {inputblob.length} bytes")
+    logging.info(f"github actions setting !!!")
     outputblob.set(inputblob)
