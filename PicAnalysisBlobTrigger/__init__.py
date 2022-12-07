03ecec73ff5fd494649eaed1c25bad2577545fa8
yosukeuchida <ee0200625@gmail.com>
2022-12-08 00:23:48 +0900

    20221207 1234

--- PicAnalysisBlobTrigger/__init__.py
+++ PicAnalysisBlobTrigger/__init__.py
@@ -31,7 +31,8 @@ def db_connection(sv=server, db=database, un=username, pw=password):
     logging.info("db", db)
     logging.info("un", un)
     logging.info("pw", pw)
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+sv+';DATABASE='+db+';UID='+un+';PWD='+ pw)
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=tohokuujaeasqlservertest1.database.windows.net;DATABASE=tohokuujaeasqldatabasetest1;UID=tohokusqlserveradmin;PWD=tohokuadmin2023@')
+    ### cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+sv+';DATABASE='+db+';UID='+un+';PWD='+ pw)
     return cnxn.cursor()
 
 ### SQLを発行
