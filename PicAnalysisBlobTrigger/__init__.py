bf278ebdc851703c5f1d49c6119992d4f3aefb30
yosukeuchida <ee0200625@gmail.com>
2022-12-08 23:36:02 +0900

    20221207 1234

--- PicAnalysisBlobTrigger/__init__.py
+++ PicAnalysisBlobTrigger/__init__.py
@@ -16,10 +16,10 @@ def main(inputblob: func.InputStream, outputblob: func.Out[func.InputStream]):
 
 ### outputblob.set(inputblob)
     
-    server = 'tcp:tohokuujaeasqlservertest1.database.windows.net' 
-    database = 'tohokuujaeasqlservertest1' 
-    username = 'tohokusqlserveradmin' 
-    password = 'tohokuadmin2023@' 
+### server = 'tcp:tohokuujaeasqlservertest1.database.windows.net' 
+### database = 'tohokuujaeasqlservertest1' 
+### username = 'tohokusqlserveradmin' 
+### password = 'tohokuadmin2023@' 
 
 ### cnxn = pyodbc.connect("Server=tcp:tohokuujaeasqlservertest1.database.windows.net,1433;Initial Catalog=tohokuujaeasqldatabasetest1;Persist Security Info=False;User ID=tohokusqlserveradmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
     cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net,1433;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
