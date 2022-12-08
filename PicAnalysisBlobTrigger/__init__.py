f06ce8bdfa1a74f85e7508d1d8e1226cb4672ab3
yosukeuchida <ee0200625@gmail.com>
2022-12-08 10:38:33 +0900

    20221207 1234

--- PicAnalysisBlobTrigger/__init__.py
+++ PicAnalysisBlobTrigger/__init__.py
@@ -1,43 +1,26 @@
 ## libraries
-import logging
 import azure.functions as func
+import logging
+import os
 import pyodbc
-
-## initial setting
-server = 'tohokuujaeasqlservertest1.database.windows.net'  
-database = 'tohokuujaeasqldatabasetest1'  
-username = 'tohokusqlserveradmin'  
-password = 'tohokuadmin2023@'
-cursor = ""
+import struct
+import sys
 
 def main(inputblob: func.InputStream, outputblob: func.Out[func.InputStream]):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {inputblob.name}\n"
                  f"Blob Size: {inputblob.length} bytes")
     logging.info(f"github actions setting !!!")
+
     outputblob.set(inputblob)
-    logging.info(f"output set !!!")
-    ### DB connectionしときます
-    cursor = db_connection()
-    logging.info(f"connection set !!!")
-    ### SalesLT.Customerはテンプレートとしてあるテーブル
-    sql = 'select count(*) from sales;' 
-    logging.info(f"sql do !!!")
-    logging.info(f"sql result : %d", query_output(sql))
     
-### DB connectionを定義
-def db_connection():    
+    server = 'tohokuujaeasqlservertest1.database.windows.net' 
+    database = 'tohokuujaeasqlservertest1' 
+    username = 'tohokusqlserveradmin' 
+    password = 'tohokuadmin2023@' 
 
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=tohokuujaeasqlservertest1.database.windows.net;DATABASE=tohokuujaeasqldatabasetest1;UID=tohokusqlserveradmin;PWD=tohokuadmin2023@')
-    ### cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+sv+';DATABASE='+db+';UID='+un+';PWD='+ pw)
-    return cnxn.cursor()
-
-### SQLを発行
-def query_output(sql):
-    cursor.execute(sql)
-    row = cursor.fetchone()
-    while row:  
-        row = cursor.fetchone()
-        logging.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-        logging.info(row[0])
-        return row[0]
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    cursor.execute("INSERT INTO Todo VALUES (?, ?, ?)", "2", "frompython", "2022-12-07T00:00:00")
+    cursor.commit()
+    logging.info('finished')
