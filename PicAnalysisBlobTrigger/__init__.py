07ebf674b34ab007b7002999d07cfebfa32dde25
yosukeuchida <ee0200625@gmail.com>
2022-12-10 01:15:13 +0900

    20221207 1234

--- PicAnalysisBlobTrigger/__init__.py
+++ PicAnalysisBlobTrigger/__init__.py
@@ -16,8 +16,8 @@ def main(inputblob: func.InputStream, outputblob: func.Out[func.InputStream]):
 ### password = 'tohokuadmin2023@' 
 
 ### cnxn = pyodbc.connect("Server=tcp:tohokuujaeasqlservertest1.database.windows.net,1433;Initial Catalog=tohokuujaeasqldatabasetest1;Persist Security Info=False;User ID=tohokusqlserveradmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
-    cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;1433;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-###    cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;")
+###    cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;1433;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;")
     logging.info("connect success !!!")
     cursor = cnxn.cursor()
 ### logging.info(database)
