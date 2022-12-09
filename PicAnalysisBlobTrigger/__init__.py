1a97f914bf5ed8482f9498432c907ec67a9e1807
yosukeuchida <ee0200625@gmail.com>
2022-12-10 02:37:52 +0900

    20221207 1234

--- PicAnalysisBlobTrigger/__init__.py
+++ PicAnalysisBlobTrigger/__init__.py
@@ -17,15 +17,16 @@ def main(inputblob: func.InputStream, outputblob: func.Out[func.InputStream]):
 
 ### cnxn = pyodbc.connect("Server=tcp:tohokuujaeasqlservertest1.database.windows.net,1433;Initial Catalog=tohokuujaeasqldatabasetest1;Persist Security Info=False;User ID=tohokusqlserveradmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
 ###    cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;1433;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-    cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tohokuujaeasqlservertest1.database.windows.net;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;')
+
+###    cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tohokuujaeasqlservertest1.database.windows.net;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;')
     logging.info("connect success !!!")
-    cursor = cnxn.cursor()
+###    cursor = cnxn.cursor()
 ### logging.info(database)
-    cursor.execute("select top(1) * from Todo")
-    while row:
+###    cursor.execute("select top(1) * from Todo")
+###    while row:
     ###print (str(row[0]) + " " + str(row[1]))
-        row = cursor.fetchone()
-        logging.info(row[0])
+###        row = cursor.fetchone()
+###        logging.info(row[0])
 ### logging.info(username)
     ###cursor.commit()
 ### logging.info(password)
