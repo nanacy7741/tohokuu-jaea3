0e1717f048af310248e7e13c63a94c2c5a55a66d
yosukeuchida <ee0200625@gmail.com>
2022-12-10 10:44:42 +0900

    20221207 1234

--- PicAnalysisBlobTrigger/__init__.py
+++ PicAnalysisBlobTrigger/__init__.py
@@ -3,38 +3,20 @@ import pyodbc
 import azure.functions as func
 
 def main(inputblob: func.InputStream, outputblob: func.Out[func.InputStream]):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {inputblob.name}\n"
-                 f"Blob Size: {inputblob.length} bytes")
-    logging.info(f"github actions setting !!!")
-
-### outputblob.set(inputblob)
     
-### server = 'tcp:tohokuujaeasqlservertest1.database.windows.net' 
-### database = 'tohokuujaeasqlservertest1' 
-### username = 'tohokusqlserveradmin' 
-### password = 'tohokuadmin2023@' 
-
+    logging.info(f"functions start !!!")
+### outputblob.set(inputblob)
 ### cnxn = pyodbc.connect("Server=tcp:tohokuujaeasqlservertest1.database.windows.net,1433;Initial Catalog=tohokuujaeasqldatabasetest1;Persist Security Info=False;User ID=tohokusqlserveradmin;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
-###    cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;1433;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+### cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;1433;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 
     cnxn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:tohokuujaeasqlservertest1.database.windows.net;Database=tohokuujaeasqldatabasetest1;Uid=tohokusqlserveradmin;Pwd=tohokuadmin2023@;')
     cursor = cnxn.cursor()
     logging.info("connect success !!!")
-###    cursor = cnxn.cursor()
-### logging.info(database)
-###    cursor.execute("select top(1) * from Todo")
-###    while row:
-    ###print (str(row[0]) + " " + str(row[1]))
-###        row = cursor.fetchone()
-###        logging.info(row[0])
-### logging.info(username)
-    ###cursor.commit()
-### logging.info(password)
+
     cursor.execute("SELECT @@version;") 
     row = cursor.fetchone() 
     while row: 
         logging.info(row[0])
         row = cursor.fetchone()
 
-    logging.info('finished')
+    logging.info('finished !!!')
